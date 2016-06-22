e5b833922d89da80835b5baf3c2b50b4cb181b53
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-06-22 15:34:04 -0700

    changed remote repo location

--- build/bundle.js
+++ build/bundle.js
@@ -57,17 +57,11 @@
 	const angular = __webpack_require__(2);
 	__webpack_require__(4);
 
-	angular.module('FoodApp', ['ngRoute'])
+	var app = angular.module('FoodApp', ['ngRoute'])
 	  .controller('MapController', ['$http', function($http) {
 	    let vm = this;
 	    vm.message = 'loading...';
 	    vm.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-	    // vm.mapOptions = {
-	    //     zoom: 4,
-	    //     center: new google.maps.LatLng(40.0000, -98.0000),
-	    //     mapTypeId: google.maps.MapTypeId.TERRAIN
-	    // }
-	    // vm.map = new google.maps.Map(document.getElementById('map'), vm.mapOptions);
 
 	    vm.getInfo = function() {
 	      $http.get('https://fb-mp-api.herokuapp.com/api/v1/services/')
@@ -75,12 +69,13 @@
 	          vm.message = res.data;
 	        }, err => console.log('GET ERROR! ', err))
 	    }
-	  }])
-	  .directive('myMaps', function(){
+	  }]);
+	  app.directive('myMaps', function(){
 	    return{
 	      restrict: 'E',
-	      template: '<div></div>',
+	      template: '<div>{{vm.message}}</div>',
 	      replace: true,
+	      scope: true,
 	      link: function(scope, element, attrs) {
 	        var myLatLng = new google.maps.LatLng(47.6062, -122.3321);
 	        var mapOptions = {
